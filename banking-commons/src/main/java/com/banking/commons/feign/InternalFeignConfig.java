f85f6b8c3cebc6f84337be955f5b2615fb8c5e0f
West One <stingion@gmail.com>
2026-03-04 20:46:28 +0100

    fix integration-test `product-service`

--- banking-commons/src/main/java/com/banking/commons/feign/InternalFeignConfig.java
+++ banking-commons/src/main/java/com/banking/commons/feign/InternalFeignConfig.java
@@ -3,12 +3,14 @@ package com.banking.commons.feign;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import org.slf4j.MDC;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.UUID;
 
 @Configuration
+@ConditionalOnClass(name = "feign.RequestInterceptor")
 public class InternalFeignConfig {
 
     private static final String CORRELATION_ID_HEADER = "X-Correlation-Id";
